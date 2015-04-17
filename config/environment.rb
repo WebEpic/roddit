936873c104ea75d327dec5838422348db4153f6d
WebEpic <randy@randyjmitchell.com>
2015-04-17 13:15:32 -0400

    corrected sendgrid code in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
@@ -7,9 +13,3 @@ ActionMailer::Base.smtp_settings = {
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
-
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-Rails.application.initialize!
