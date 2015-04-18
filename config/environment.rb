c0a97b878101387907955b71c460f8eefb549375
WebEpic <randy@randyjmitchell.com>
2015-04-17 20:32:49 -0400

    devise sendgrid heroku config still not working

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Still working on this integration -- configuring Devise to work with ActionMailer.
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
